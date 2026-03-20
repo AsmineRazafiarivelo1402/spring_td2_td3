45dd1f8ab46be3d9690ba2df6619e678edfe0ce1
Miriasme <johndoe@example.com>
2026-03-20 13:02:46 +0300

    feat: TD2:QC

--- spring_td2_td3/src/main/java/org/hei/spring_td2_td3/WelcomeController.java
+++ spring_td2_td3/src/main/java/org/hei/spring_td2_td3/WelcomeController.java
@@ -25,4 +25,21 @@ public class WelcomeController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(noms);
     }
+
+    @GetMapping("/students")
+    public ResponseEntity<String> getStudents(@RequestHeader("Accept") String accept) {
+
+        if ("text/plain".equals(accept)) {
+            String noms = students.stream()
+                    .map(Student::getFirstName)
+                    .collect(Collectors.joining(", "));
+            return ResponseEntity.ok()
+                    .header("Content-Type", "text/plain")
+                    .body(noms);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_ACCEPTABLE)
+                    .body("Format non supporté.");
+        }
+    }
 }
