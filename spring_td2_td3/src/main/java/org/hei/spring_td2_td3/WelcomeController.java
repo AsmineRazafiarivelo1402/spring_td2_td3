f8279ba89554cb860ff93a23f372b6f3d13e1a81
Miriasme <johndoe@example.com>
2026-03-20 08:03:33 +0300

    feat: TD2_QB

--- spring_td2_td3/src/main/java/org/hei/spring_td2_td3/WelcomeController.java
+++ spring_td2_td3/src/main/java/org/hei/spring_td2_td3/WelcomeController.java
@@ -1,12 +1,28 @@
 package org.hei.spring_td2_td3;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class WelcomeController {
     @GetMapping("/welcome")
     public String welcome(@RequestParam("name") String name) {
         return "Welcome " + name;
+    }
+
+    private final List<Student> students = new ArrayList<>();
+    @PostMapping("/students")
+    public ResponseEntity<String> createStudent(@RequestBody List<Student> studentList) {
+        students.addAll(studentList);
+        String noms = students.stream()
+                .map(Student::getFirstName)
+                .collect(Collectors.joining(", "));
+
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(noms);
     }
 }
