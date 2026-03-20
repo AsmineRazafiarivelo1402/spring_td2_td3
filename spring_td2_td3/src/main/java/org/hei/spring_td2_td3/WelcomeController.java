7dd6d4ee1cc20228d0b9f71c6494830b668323ad
Miriasme <johndoe@example.com>
2026-03-20 14:59:14 +0300

    feat: TD3:QAb

--- spring_td2_td3/src/main/java/org/hei/spring_td2_td3/WelcomeController.java
+++ spring_td2_td3/src/main/java/org/hei/spring_td2_td3/WelcomeController.java
@@ -3,9 +3,12 @@ package org.hei.spring_td2_td3;
 import jdk.jshell.Snippet;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class WelcomeController {
@@ -17,4 +20,17 @@ public class WelcomeController {
 
         return ResponseEntity.ok("Welcome " + name);
     }
-}
+
+    private final List<Student> students = new ArrayList<>();
+    @PostMapping("/studentspost")
+    public ResponseEntity<?> createStudent(@RequestBody List<Student> studentList) {
+        try {
+            students.addAll(studentList);
+            return ResponseEntity.status(HttpStatus.CREATED).body(students);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                   HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        }
+    }
+
