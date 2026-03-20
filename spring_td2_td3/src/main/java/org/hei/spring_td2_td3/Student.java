551027ec62111edd4df762ff7394a527bb5827f7
Miriasme <johndoe@example.com>
2026-03-20 07:31:58 +0300

    feat: add class Student

---
+++ spring_td2_td3/src/main/java/org/hei/spring_td2_td3/Student.java
@@ -0,0 +1,17 @@
+package org.hei.spring_td2_td3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    private String reference;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+}
